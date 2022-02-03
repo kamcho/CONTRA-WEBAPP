078b644949594b6affbbe624e9860c05cc2ebf53
kamcho <61869599+kamcho@users.noreply.github.com>
2022-02-03 15:19:29 +0300

    Add files via upload

---
+++ Home/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+# from .models import Quizes,Preference
+#
+#
+# admin.site.register(Quizes)
+# admin.site.register(Preference)
+
+
+
+# Register your models here.
---
+++ Home/kbs.py
@@ -0,0 +1,8 @@
+
+IUD = ["copper_allergy","cancer","preggo","infection"]
+
+P2 = ["BP","stroke","HA","clot"]
+
+C_injection = ["preggo","baby","liver","B_cancer","heart_disease"]
+
+pullout = ["PE"]
